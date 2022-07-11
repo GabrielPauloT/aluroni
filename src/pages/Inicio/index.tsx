67aabb7a70b539d108db8e6dcd625b53ae6ecb71
{Gabriel Paulo} <gabrielpt201111@hotmail.com>
2022-07-11 15:28:49 -0300

    Adicionando Telas Inicio/Sobre

---
+++ src/pages/Inicio/index.tsx
@@ -0,0 +1,35 @@
+import cardapio from 'data/cardapio.json';
+import styles from './Inicio.module.scss';
+import stylesTema from 'styles/Tema.module.scss';
+import nossaCasa from 'assets/nossa_casa.png';
+
+export default function Inicio() {
+  let pratosRecomendados = [...cardapio];
+  pratosRecomendados = pratosRecomendados.sort(() => 0.5 - Math.random()).splice(0,3);
+  return (
+    <section>
+      <h3 className={stylesTema.titulo}>
+        Recomendações da conzinha
+      </h3>
+      <div className={styles.recomendados}>
+        {pratosRecomendados.map(item => (
+          <div key={item.id} className={styles.recomendado}>
+            <div className={styles.recomendado__imagem}>
+              <img src={item.photo} alt={item.title} />
+            </div>
+            <button className={styles.recomendado__botao} >
+              Ver mais
+            </button>
+          </div>
+        ))}
+      </div>
+      <h3 className={stylesTema.titulo}> Nossa Casa </h3>
+      <div className={styles.nossaCasa}>
+        <img src={nossaCasa} alt="Casa do Aluroni" />
+        <div className={styles.nossaCasa__endereco}>
+          Rua Nito de deus Vieira, 112 <br /> Apartamento 301 <br /> <br /> Patos de Minas - MG
+        </div>
+      </div>
+    </section>
+  );
+}
